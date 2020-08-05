2231bf192c4c77217b9245578cc581b4a23510bb
Lars Isenberg <isenberg.lars@gmail.com>
2020-08-05 16:59:29 +0200

    Made createMeanSdTable work

--- R/createMeanSdTable.R
+++ R/createMeanSdTable.R
@@ -1,18 +1,18 @@
 ########################################################################################################################
 createMeanSdTable <- function(data, moving_average = NULL) {
     # calculate daily mean and sd for all years up the most recent one
-    mean_sd_table <- data[, .(
-            value = mean(value, na.rm = TRUE),
-            sd = sd(value, na.rm = TRUE)),
-        by = .(Datum = yday(Datum), variable)]
-    setkey(mean_sd_table, variable, Datum)
-    
-    mean_sd_table <- mean_sd_table[Datum != 366] # removal of extra leap day (often strange values)
+    mean_sd_table <- data %>%
+        mutate(Datum = lubridate::yday(Datum),
+               value_sd = value) %>% # Added as dplyr can't summarise multiple times from a single column
+        group_by(Datum, variable) %>%
+        summarise(mean_value = mean(value, na.rm = TRUE),
+                  sd_value = sd(value_sd, na.rm = TRUE)) %>%
+        filter(Datum != 366) # removal of extra leap day (often strange values)
+
     if (is.numeric(moving_average)) {
-        mean_sd_table[, ":=" (
-                value = rollapply(value, width = moving_average, FUN = mean, na.rm = TRUE, partial = TRUE),
-                sd = rollapply(sd, width = moving_average, FUN = mean, na.rm = TRUE, partial = TRUE)),
-            by = .(variable)]
+        mean_sd_table <- mean_sd_table %>%
+            group_by(variable) %>%
+            mutate_at(vars(mean_value, sd_value), zoo::rollapply, width = moving_average, FUN = mean, na.rm = TRUE, partial = TRUE)
     }
     return(mean_sd_table)
 }
