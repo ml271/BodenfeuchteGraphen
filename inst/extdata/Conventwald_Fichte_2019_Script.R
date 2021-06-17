44c685d709ef09d94b1e0fe6ed8b888266df270d
Lars Isenberg <isenberg.lars@gmail.com>
2021-06-17 17:00:38 +0200

    inst script for Conventwald Fichte 2019 should be adjusted for changes in the functions now (Needs to be tested)

--- inst/extdata/Conventwald_Fichte_2019_Script.R
+++ inst/extdata/Conventwald_Fichte_2019_Script.R
@@ -1,49 +1,51 @@
-# ########################################################################################################################
-# plot <- "Conventwald"
-# sub_plot <- "Fichte"
-# data_table <- loadCorrectedAndRawData(plot, sub_plot)
-#
-#
-# ########################################################################################################################
-# # Dropping faulty data
-# data_table <- data_table %>%
-#     filter(value <= 100) %>%
-#     filter(variable != "60_FDR_Z")
-#
-#
-# ########################################################################################################################
-# # Create Graphs and manually fix data from them
-# raw_graph_path <- file.path(getwd(), "Graphs")
-# #createRawGraphs(as.data.table(data_table), raw_graph_path)
-# #createCombiGraphs(as.data.table(data_table), raw_graph_path)
-#
-# # Drop positions
-# data_table <- data_table %>%
-#     mutate(variable = str_match(variable, pattern = "[0-9]{2}"))
-#
-# out_path <- file.path(raw_graph_path, "..", "..")
-# createCompletePlot(data_table,
-#     selected_variable = "15",
-#     target_years = target_years,
-#     out_path,
-#     limits = global_limits,
-#     file_extension = global_file_type,
-#     moving_average = moving_average)
-#
-# createCompletePlot(data_table,
-#     selected_variable = "30",
-#     target_years = target_years,
-#     out_path,
-#     limits = global_limits,
-#     file_extension = global_file_type,
-#     moving_average = moving_average)
-#
-# createCompletePlot(data_table,
-#     selected_variable = "60",
-#     target_years = target_years,
-#     out_path,
-#     limits = global_limits,
-#     file_extension = global_file_type,
-#     moving_average = moving_average)
-#
-#
+########################################################################################################################
+plot_name <- "Conventwald"
+sub_plot_name <- "Fichte"
+data_table <- loadCorrectedAndRawData(level2_path, plot_name, sub_plot_name)
+
+
+
+
+########################################################################################################################
+# Dropping faulty data
+filtered_data <- data_table %>%
+    filter(value > 0) %>%
+    filter(value < 100) %>%
+    filter(!(Datum >= as.POSIXct("2019-01-01", tz = "UTC") & variable == "60_FDR_Z")) %>%
+    filter(!(Datum >= as.POSIXct("2019-09-03", tz = "UTC") & variable == "15_FDR_X")) %>%
+    mutate(variable = stringr::str_replace(variable, "KR", "Y"))
+
+
+########################################################################################################################
+# Create Graphs and manually fix data from them
+out_path <- file.path("graphs")
+raw_graph_path <- file.path(out_path, plot_name, sub_plot_name)
+# createCombiGraphs(as.data.table(filtered_data), raw_graph_path)
+# createRawGraphs(as.data.table(filtered_data), raw_graph_path)
+
+
+# Drop positions
+co_15 <- createCompletePlot(filtered_data,
+                   selected_variable = "15",
+                   target_years = target_years,
+                   limits = global_limits,
+                   file_extension = global_file_type,
+                   moving_average = moving_average)
+ggsave(file.path(out_path, "Conventwald_Fichte_15_cm_2019_2020.png"), co_15)
+
+co_30 <- createCompletePlot(filtered_data,
+                   selected_variable = "30",
+                   target_years = target_years,
+                   limits = global_limits,
+                   file_extension = global_file_type,
+                   moving_average = moving_average)
+ggsave(file.path(out_path, "Conventwald_Fichte_30_cm_2019_2020.png"), co_30)
+
+co_60 <- createCompletePlot(filtered_data,
+                   selected_variable = "60",
+                   target_years = target_years,
+                   limits = global_limits,
+                   file_extension = global_file_type,
+                   moving_average = moving_average)
+ggsave(file.path(out_path, "Conventwald_Fichte_60_cm_2019_2020.png"), co_60)
+
