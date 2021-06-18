3419dc74c0d35c275d4f2a7c05039f59b47e0830
Lars Isenberg <isenberg.lars@gmail.com>
2021-06-18 14:49:43 +0200

    fixed loadCorrectedAndRawData so it works with the current S4Level2 Pakage

--- R/loadCorrectedAndRawData.R
+++ R/loadCorrectedAndRawData.R
@@ -1,19 +1,19 @@
 ########################################################################################################################
 loadCorrectedAndRawData <- function(level2_path, plot_name, sub_plot_name) {
-    level2_plot <- S4Level2::loadL2Object(level2_path) %>%
-        S4Level2::getObjectByURI(plot_name)
-    al_corrected_data <- level2_plot %>%
-        S4Level2::loadCorrectedData(sub_plot_name) %>%
+    al_corrected_data <- S4Level2::loadCorrectedData(plot_name, sub_plot_name) %>%
         filter(stringr::str_detect(variable, "^[0-9]{2}_FDR")) %>%
         filter(!is.na(value)) %>%
         mutate(Plot = plot_name)
 
     max_corrected_datum <- al_corrected_data %>%
-        summarise(max = max(Datum)) %>%
-        pull()
+        pull(Datum) %>%
+        max()
 
-    raw_data <- level2_plot %>%
-        S4Level2::getData(start.date = max_corrected_datum + 60, sub.plot = sub_plot_name) %>%
+    raw_data <- S4Level2::getData(
+        start_date = max_corrected_datum + 60,
+        end_date = "2300-01-01",
+        plot = plot_name,
+        sub_plot = sub_plot_name) %>%
         select(-Logger) %>%
         filter(stringr::str_detect(variable, "^[0-9]{2}_FDR")) %>%
         filter(!is.na(value))
